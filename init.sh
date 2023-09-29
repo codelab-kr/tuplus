8abafaabf28a3f8cd7efcf75cb76ff8979616e0d
codelab-kr <master@code-lab.kr>
2023-09-29 23:08:01 +0900

    Add sub modules

--- init.sh
+++ init.sh
@@ -1,8 +1,31 @@
-# git pull
-echo "tuflix" && npm install
-echo "oci-storage" && cd oci-storage && npm install && cd ..
-echo "gateway" && cd gateway && npm install && cd ..
-echo "metadata" && cd metadata && npm install && cd ..
-echo "video-streaming" && cd video-streaming && npm install && cd ..
-echo "video-upload" && cd video-upload && npm install && cd ..
-echo "history" && cd history && npm install && cd ..
+git submodule add https://github.com/codelab-kr/tuplus-db-fixture-rest-api.git db-fixture-rest-api
+git submodule add https://github.com/codelab-kr/tuplus-history.git history
+git submodule add https://github.com/codelab-kr/tuplus-gateway.git gateway
+git submodule add https://github.com/codelab-kr/tuplus-metadata.git metadata
+git submodule add https://github.com/codelab-kr/tuplus-mock-storage.git mock-storage
+git submodule add https://github.com/codelab-kr/tuplus-oci-storage.git oci-storage
+git submodule add https://github.com/codelab-kr/tuplus-security.git security
+git submodule add https://github.com/codelab-kr/tuplus-terraform.git terraform
+git submodule add https://github.com/codelab-kr/tuplus-video-streaming.git video-streaming
+git submodule add https://github.com/codelab-kr/tuplus-video-upload.git video-upload
+
+
+
+# db-fixture-rest-api
+# gateway
+# history
+# metadata
+# mock-storage
+# oci-storage
+# security
+# terraform
+# video-streaming
+# video-upload
+
+# echo "tuflix" && npm install
+# echo "oci-storage" && cd oci-storage && npm install
+# echo "gateway" && cd gateway && npm install
+# echo "metadata" && cd metadata && npm install
+# echo "video-streaming" && cd video-streaming && npm install
+# echo "video-upload" && cd video-upload && npm install
+# echo "history" && cd history && npm install
