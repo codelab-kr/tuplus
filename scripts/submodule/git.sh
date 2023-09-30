88d386971dbf03d4d46e10a1f7ddbdaf4f7d5fe9
codelab-kr <master@code-lab.kr>
2023-10-01 01:06:09 +0900

    test git.sh

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -1,8 +1,38 @@
-git submodule foreach \
-'if [[ $(git status --porcelain) && $msg ]]; \
-then git add . | 2>/dev/null && git commit -m $msg && git push; \
-fi;' -- $msg=test
+# git submodule foreach 'echo $path $var' -- $var='test'
 
-if [[ $(git status --porcelain) && $1 ]]; \
-then git add . && git commit -m "$1" && git push && echo "tuplus git add & commit & push"; \
+
+git submodule foreach -q \
+'if [[ $(git status --porcelain) ]]; then \
+  git add . | 2>/dev/null && git commit -m "$msg" && git push && echo "$path git"; \
+fi;'$1 || :
+
+if [[ $(git status --porcelain) && $1 ]]; then \
+  git add . && git commit -m "$1" && git push && echo "tuplus git add & commit & push"; \
 fi;
+
+# then git add . | 2>/dev/null && git commit -m $msg && git push; \
+
+# git submodule foreach \
+# 'if [[ $(git status --porcelain) && $var ]]; \
+#   then echo "$path $var"; \
+# fi;' -- $var=test
+
+
+# git submodule foreach --recursive '[[ "$name" = *"foo"* ]] && \
+#      ( echo $path; echo "paths+=($path)" >> /tmp/paths ) || true'
+# source /tmp/paths
+# rm /tmp/paths
+# echo ${paths[@]}
+
+# git submodule foreach --recursive "git checkout $user_branch" | while read -r path; do
+#   echo $path
+# done
+
+# git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; \
+#   if [[ "$branch" = "master" ]]; \
+#   then echo $path; \
+#   fi;' | while read -r path; do
+#     echo $path
+#   done
+
+git submodule foreach 'echo "$msg"' $1
--- scripts/submodule/hooks-path.sh
+++ scripts/submodule/hooks-path.sh
@@ -1,6 +1,6 @@
-git submodule foreach \
+git submodule foreach -q \
 'path=$(echo $path | cut -d/ -f2) && \
 if [ "$path" != "db-fixture-rest-api" ]; \
 then git config core.hooksPath ../.husky && \
-echo "$path changed hooksPath to ../.husky"; \
-fi;'
+echo "$path hooksPath changed"; \
+fi;' || :
