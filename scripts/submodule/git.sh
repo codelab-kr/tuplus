d8010b4e650c6ea0881f4e4696a825bf01c9abda
codelab-kr <master@code-lab.kr>
2023-10-01 00:20:47 +0900

    test git.sh

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -1,8 +1,7 @@
 git submodule foreach \
 'if [[ $(git status --porcelain) && $msg ]]; then \
   git add . | 2>/dev/null && git commit -m "$msg" && git push && echo "$path git add & commit & push"; \
-fi;' \
--- $msg="$1"
+fi;' -- $msg=$1
 
 if [[ $(git status --porcelain) && $1 ]]; \
 then git add . && git commit -m "$1" && git push && echo "tuplus git add & commit & push"; \
