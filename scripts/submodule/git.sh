ddfa9b5dd8249b83fb6bcd6d8d132e0fce66f843
codelab-kr <master@code-lab.kr>
2023-09-30 23:46:18 +0900

    test git.sh

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -1,6 +1,5 @@
 git submodule foreach \
-'path=$(echo $path | cut -d/ -f2); \
-if [[ $(git status --porcelain) && $1 ]]; \
+'if [ $(git status --porcelain) && $1 ]; \
 then git add . && git commit -m "$1" && git push && echo "$path git add & commit & push"; \
 fi;'
 
