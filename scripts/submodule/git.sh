f5428be934d0d6e52ded6729065351b9c56f79fe
codelab-kr <master@code-lab.kr>
2023-10-01 22:05:23 +0900

    Webhook test

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-msg=$1 
+msg=$@
 
 if [[ -z "$msg" ]]; then
   echo "Commit message is required"
@@ -12,7 +12,7 @@ git submodule foreach --recursive \
 'if [[ $(git status --porcelain) ]]; then \
   commit_msg="$@" && echo "$commit_msg" && \
   git add . && git commit -m "$commit_msg" && git push; \
-fi;' $msg || :;
+fi;' $msg;
 
 echo "\n======== Main Repo Update ========"
 if [[ $(git status --porcelain) ]]; then \
