2281093543963fc6d0bcb13e562731a3ec206da1
codelab-kr <master@code-lab.kr>
2023-10-01 21:31:57 +0900

    Test git.sh

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-msg=$1
-echo "msg: $msg"
+msg=$1 
 
 if [[ -z "$msg" ]]; then
-  msg="tuplus submodule update default commit message";
+  echo "msg is empty"
+  exit 1
 fi;
 
-dd=`if [[ $(git status --porcelain) ]]; then \
+echo "msg: $msg"
+
+# $1이 존재할 경우에만 잘 실행되고 그렇지 않으면 command not found 에러가 발생한다.
+# git submodule foreach --recursive \
+# 'if [[ $(git status --porcelain) ]]; then \
+#   commit_msg="$@" && echo "$commit_msg" && \
+#   git add . && git commit -m "$commit_msg" && git push && echo "$commit_msg"; \
+# fi;' $msg;
+
+
+git submodule foreach --recursive \
+'if [[ $(git status --porcelain) ]]; then \
+  commit_msg="$@" && echo "$commit_msg" && \
+  git add . && git commit -m "$commit_msg" && git push && echo "$commit_msg"; \
+fi;' $msg;
+
+
+if [[ $(git status --porcelain) ]]; then \
   git add . && git commit -m "$msg" && git push && echo "$msg"; \
-fi;`
-
-git submodule foreach "$dd" "$msg" || :
-
+fi;
