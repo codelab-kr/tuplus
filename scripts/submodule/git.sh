ac7b9cee0d4755094f51476c248a7ec1760c1477
codelab-kr <master@code-lab.kr>
2023-10-01 18:54:27 +0900

    Test git.sh

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
 msg=$1
-echo "msg: $msg"
-echo "COMMIT_MSG: $COMMIT_MSG"
-
-msg=${COMMIT_MSG:-$msg}
-
 echo "msg: $msg"
 
 if [[ -z "$msg" ]]; then
   msg="tuplus submodule update default commit message";
 fi;
 
+git submodule foreach \
+'if [[ $(git status --porcelain) ]]; then \
+    git add . && git commit -m "$msg" && git push && echo "$msg"; \
+fi;' || :
+
 if [[ $(git status --porcelain) ]]; then \
- git add . && git commit -m "$msg" && git push && echo "$msg"; \
+  git add . && git commit -m "$msg" && git push && echo "$msg"; \
 fi;
