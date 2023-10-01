0f60b3459badc5cd825526aa4e0440183ebb2068
codelab-kr <master@code-lab.kr>
2023-10-01 21:35:24 +0900

    Test git.sh

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -3,27 +3,20 @@
 msg=$1 
 
 if [[ -z "$msg" ]]; then
-  echo "msg is empty"
+  echo "Commit message is required"
   exit 1
 fi;
 
-echo "msg: $msg"
-
-# $1이 존재할 경우에만 잘 실행되고 그렇지 않으면 command not found 에러가 발생한다.
-# git submodule foreach --recursive \
-# 'if [[ $(git status --porcelain) ]]; then \
-#   commit_msg="$@" && echo "$commit_msg" && \
-#   git add . && git commit -m "$commit_msg" && git push && echo "$commit_msg"; \
-# fi;' $msg;
-
-
+# submodule repo update
+echo "======== Submodule repo update ========"
 git submodule foreach --recursive \
 'if [[ $(git status --porcelain) ]]; then \
   commit_msg="$@" && echo "$commit_msg" && \
   git add . && git commit -m "$commit_msg" && git push && echo "$commit_msg"; \
 fi;' $msg;
 
-
+echo "======== Main repo update ========"
+# main repo update
 if [[ $(git status --porcelain) ]]; then \
   git add . && git commit -m "$msg" && git push && echo "$msg"; \
 fi;
