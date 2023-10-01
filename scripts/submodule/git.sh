d839daa870e680eeb222aba58550b2dd2265d1df
codelab-kr <master@code-lab.kr>
2023-10-01 22:10:52 +0900

    Webhook test

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -7,12 +7,20 @@ if [[ -z "$msg" ]]; then
   exit 1
 fi;
 
+
 echo "======== Submodule Repo Update ========"
 git submodule foreach --recursive \
-'if [[ $(git status --porcelain) ]]; then \
-  commit_msg="$@" && echo "$commit_msg" && \
-  git add . && git commit -m "$commit_msg" && git push; \
-fi;' "$(echo "$@")" || :;
+"if [[ \$(git status --porcelain) ]]; then \
+  commit_msg=\"\$@\" && echo \"\$commit_msg\" \
+  git add . && git commit -m \"\$commit_msg\" && git push && echo \"\$commit_msg\"; \
+fi;" "$(echo "$msg")" || :
+
+# echo "======== Submodule Repo Update ========"
+# git submodule foreach --recursive \
+# 'if [[ $(git status --porcelain) ]]; then \
+#   commit_msg="$@" && echo "$commit_msg" && \
+#   git add . && git commit -m "$commit_msg" && git push; \
+# fi;' "$(echo "$@")" || :;
 
 echo "\n======== Main Repo Update ========"
 if [[ $(git status --porcelain) ]]; then \
