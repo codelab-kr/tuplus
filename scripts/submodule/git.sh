fb60bfacdc315a742d72bf2126f2f910aa682578
codelab-kr <master@code-lab.kr>
2023-10-01 15:14:05 +0900

    test commit

--- scripts/submodule/git.sh
+++ scripts/submodule/git.sh
@@ -1,38 +1,9 @@
-# git submodule foreach 'echo $path $var' -- $var='test'
+msg=$1
 
-
-git submodule foreach -q \
-'if [[ $(git status --porcelain) ]]; then \
-  git add . | 2>/dev/null && git commit -m "$msg" && git push && echo "$path git"; \
-fi;'$1 || :
-
-if [[ $(git status --porcelain) && $1 ]]; then \
-  git add . && git commit -m "$1" && git push && echo "tuplus git add & commit & push"; \
+if [[ -z "$msg" ]]; then
+  msg="tuplus submodule update default commit message";
 fi;
 
-# then git add . | 2>/dev/null && git commit -m $msg && git push; \
-
-# git submodule foreach \
-# 'if [[ $(git status --porcelain) && $var ]]; \
-#   then echo "$path $var"; \
-# fi;' -- $var=test
-
-
-# git submodule foreach --recursive '[[ "$name" = *"foo"* ]] && \
-#      ( echo $path; echo "paths+=($path)" >> /tmp/paths ) || true'
-# source /tmp/paths
-# rm /tmp/paths
-# echo ${paths[@]}
-
-# git submodule foreach --recursive "git checkout $user_branch" | while read -r path; do
-#   echo $path
-# done
-
-# git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; \
-#   if [[ "$branch" = "master" ]]; \
-#   then echo $path; \
-#   fi;' | while read -r path; do
-#     echo $path
-#   done
-
-git submodule foreach 'echo "$msg"' $1
+if [[ $(git status --porcelain) ]]; then \
+ git add . && git commit -m "$msg" && git push && echo "$msg"; \
+fi;
