e0349154fc0a66a15b0d5b539045eb837973ba76
codelab-kr <master@code-lab.kr>
2023-10-02 15:50:06 +0900

    Delete scripts

--- scripts/submodule/hooks-path.sh
+++
@@ -1,6 +0,0 @@
-git submodule foreach -q \
-'path=$(echo $path | cut -d/ -f2) && \
-if [ "$path" != "db-fixture-rest-api" ]; \
-then git config core.hooksPath ../.husky && \
-echo "$path hooksPath changed"; \
-fi;' || :
