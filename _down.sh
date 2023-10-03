6fc61692380a3b638ecfe3d012ba9c88f0b87e5e
codelab-kr <master@code-lab.kr>
2023-10-03 23:20:45 +0900

    [CHG] container registry to azure

--- _down.sh
+++ _down.sh
@@ -1,13 +1,4 @@
-export DOCKER_BUILDKIT=0
-
 # docker-compose
 if docker-compose ps > /dev/null; then
   docker-compose down -v --remove-orphans
 fi
-
-if [[ $1 && "$1" -eq "dev" ]]; then
-  docker-compose -f docker-compose.dev.yaml up --build
-else
-  docker-compose up --build
-fi
-
